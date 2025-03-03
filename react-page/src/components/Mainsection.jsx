f7f4073b63eef1750c0d41631bbd328e93f59ded
mwambutsa05 <mwambutsadaryce@gmail.com>
2025-03-03 10:22:07 +0200

    change of hero section Image

--- react-page/src/components/Mainsection.jsx
+++ react-page/src/components/Mainsection.jsx
@@ -12,7 +12,7 @@ import cocktails from "../components/Assets/Passions_cocktails.jpg";
 import drinks from "../components/Assets/liquor_cabine.jpg";
 import cabin from "../components/Assets/liquor_rocks.jpg";
 import house from "../components/Assets/house.jpg";
-import red from "../components/Assets/red_cocktail.jpg"
+import red from "../components/Assets/red_cocktail.jpg";
 function Mainsection() {
   return (
     <div className="main">
@@ -22,80 +22,92 @@ function Mainsection() {
           <h1>Our Events Lineup</h1>
         </div>
         <div className="main-divs">
-          <div>
+          <section>
             <img src={firstImg} alt="description" />
-            <h2>
-              <strong>Hidden Nights</strong>
-            </h2>
-            <p>
-              Saturday, December 3, 2025 <strong>.</strong> 7pm - 11pm
-            </p>
-            <div className="btn-1">
-              <button type="button">Book Now</button>
+            <div>
+              <h2>
+                <strong>Hidden Nights</strong>
+              </h2>
+              <p>
+                Saturday, December 3, 2025 <strong>.</strong> 7pm - 11pm
+              </p>
+              <div className="btn-1">
+                <button type="button">Book Now</button>
+              </div>
             </div>
-          </div>
-          <div>
+          </section>
+          <section>
             <img src={secondImg} alt="description" />
-            <h2>
-              <strong>Shining Sparkled</strong>
-            </h2>
-            <p>
-              Saturday, December 7, 2025 <strong>.</strong> 7pm - 11pm
-            </p>
-            <div className="btn-2">
-              <button type="button">Book Now</button>
+            <div>
+              <h2>
+                <strong>Shining Sparkled</strong>
+              </h2>
+              <p>
+                Saturday, December 7, 2025 <strong>.</strong> 7pm - 11pm
+              </p>
+              <div className="btn-2">
+                <button type="button">Book Now</button>
+              </div>
             </div>
-          </div>
-          <div>
+          </section>
+          <section>
             <img src={thirdImg} alt="description" />
-            <h2>
-              <strong>Hidden Nights</strong>
-            </h2>
-            <p>
-              Saturday, December 15, 2025 <strong>.</strong> 7pm - 11pm
-            </p>
-            <div className="btn-3">
-              <button type="button">Book Now</button>
+            <div>
+              <h2>
+                <strong>Hidden Nights</strong>
+              </h2>
+              <p>
+                Saturday, December 15, 2025 <strong>.</strong> 7pm - 11pm
+              </p>
+              <div className="btn-3">
+                <button type="button">Book Now</button>
+              </div>
             </div>
-          </div>
-          <div>
+          </section>
+          <section>
             <img src={FourthImg} alt="description" />
-            <h2>
-              <strong>Shining Sparkled</strong>
-            </h2>
-            <p>
-              Saturday, December 24, 2025 <strong>.</strong> 7pm - 11pm
-            </p>
-            <div className="btn-4">
-              <button type="button">Book Now</button>
+            <div>
+              <h2>
+                <strong>Shining Sparkled</strong>
+              </h2>
+              <p>
+                Saturday, December 24, 2025 <strong>.</strong> 7pm - 11pm
+              </p>
+              <div className="btn-4">
+                <button type="button">Book Now</button>
+              </div>
             </div>
-          </div>
+          </section>
         </div>
       </div>
       <div className="second_section">
         <h3>EVENT SPACES</h3>
         <h1>Private and Special events</h1>
-        <div className="riverside">
+        <section>
           <img src={firthImg} alt="description" />
-          <h3>Indoor</h3>
-          <h1>Riverside Garden</h1>
-          <p>
-          A serene escape by the water, Riverside Garden offers lush greenery,
-          peaceful walkways, a refreshing connection to nature.
-          </p>
-           <button type="button">Reserve Now</button>
-        </div>
-        <div className="atrium">
+          <div className="riverside">
+            <h3>Indoor</h3>
+            <h1>Riverside Garden</h1>
+            <p>
+              A serene escape by the water, Riverside Garden offers lush
+              greenery, peaceful walkways, a refreshing connection to nature.
+            </p>
+            <button type="button">Reserve Now</button>
+          </div>
+        </section>
+        <section className="atr">
+          <div className="atrium">
+            <h3>Food</h3>
+            <h1>Atrium Backbar</h1>
+            <p>
+              Atrium backbar is a sleek and functional bar setup designed for
+              efficiency and style, It enhances workflow while adding a touch of
+              sophisticated to any space.
+            </p>
+            <button type="button">Reserve Now</button>
+          </div>
           <img src={wedImg} alt="description" />
-          <h3>Food</h3>
-          <h1>Atrium Backbar</h1>
-          <p>
-            Atrium backbar is a sleek and functional bar setup designed for
-            efficiency and style, It enhances workflow while adding a touch of
-            sophisticated to any space.
-          </p>
-           <button type="button">Reserve Now</button>
-        </div>
+        </section>
       </div>
       <div className="third_section">
         <h3>SOCIAL MEDIA</h3>
